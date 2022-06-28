166e5f53745344b21ac8b16c22d5b7abe1981171
Igor <leonidlee777@gmail.com>
2022-06-28 16:55:34 +0300

    fixing store, add toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+
 import createSagaMiddleware from 'redux-saga';
+import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from '../reducers/index';
 import rootSaga from '../saga/countSaga'
 
 const sagaMiddleware = createSagaMiddleware();
 
-// const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-const store = createStore(reducer, compose(applyMiddleware(sagaMiddleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = configureStore({
+    reducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(sagaMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+})
 
 sagaMiddleware.run(rootSaga)
 

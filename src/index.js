b4b75a00c3fc87c8aca01e875e94d32b1f96d734
Igor <leonidlee777@gmail.com>
2022-11-08 19:57:15 +0200

    fixing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter} from "react-router-dom";
 
 import App from './components/app/App';
 import store from './store/index'
@@ -9,7 +10,9 @@ import store from './store/index'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</Provider>
 );
 

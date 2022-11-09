1862f72f4befa05a2532618eea0c0e098912a8e9
Igor <leonidlee777@gmail.com>
2022-11-09 08:42:38 +0200

    fix modal scroll

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 
 import MainPage from '../pages/MainPage';
 import TaskChartPage from "../pages/TaskChartPage";
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,9 +4,14 @@ import Timer from "../timer/Timer";
 import TableHeader from "../tableHeader/TableHeader";
 import Modal from "../modal/Modal";
 import TaskLog from "../tasksLog/TaskLog";
+import { useEffect } from "react";
 
 const MainPage = () => {
     const { modal } = useSelector(state => state);
+
+    useEffect(() => {
+        document.body.style.overflow = modal ? 'hidden' : 'scroll'
+    },[modal])
     
     return (
         <>
